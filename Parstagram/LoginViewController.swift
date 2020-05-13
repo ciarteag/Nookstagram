e25d932b01434db44d84319a39c109fbdd7ff20f
Carlos Arteaga <ciarteag@uci.edu>
2020-05-13 09:57:51 -0700

    added logging in, launching camera, sending posts, and load posts (small error in loading posts)

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -1,8 +1,9 @@
 //
 //  LoginViewController.swift
-//  
+//  Parstagram
 //
 //  Created by Carlos Arteaga on 5/12/20.
+//  Copyright © 2020 Carlos Arteaga. All rights reserved.
 //
 
 import UIKit
@@ -10,42 +11,54 @@ import Parse
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var usernameField: UITextField!
-    @IBOutlet weak var passwordField: UITextField!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+     @IBOutlet weak var usernameField: UITextField!
+       
+       @IBOutlet weak var passwordField: UITextField!
+       
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func onSignIn(_ sender: Any) {
-    }
-    
-    @IBAction func onSignUp(_ sender: Any) {
-        let user = PFUser()
-        user.username = usernameField.text
-        user.password = passwordField.text
+           // Do any additional setup after loading the view.
+       }
+       
+       @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
         
-        user.signUpInBackground { (success, error) in
-            if success{
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
                 self.performSegue(withIdentifier: "loginSegue", sender: nil)
             }
-            else {
+            else{
                 print("Error: \(error)")
             }
         }
-    }
-    
-    /*
-    // MARK: - Navigation
+       }
+       
+       @IBAction func onSignUp(_ sender: Any) {
+           let user = PFUser()
+           user.username = usernameField.text
+           user.password = passwordField.text
+           
+           user.signUpInBackground { (success, error) in
+               if success{
+                   self.performSegue(withIdentifier: "loginSegue", sender: nil)
+               }
+               else {
+                   print("Error: \(error)")
+               }
+           }
+       }
+       
+       /*
+       // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+       // In a storyboard-based application, you will often want to do a little preparation before navigation
+       override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           // Get the new view controller using segue.destination.
+           // Pass the selected object to the new view controller.
+       }
+       */
 
 }
